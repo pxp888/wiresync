555c8901e0da98720957a3519c64818334c0b9fc
pxp888 <pxperrine@gmail.com>
2024-01-27 01:33:18 +0100

    Update welcome page and add POST request handling in views.py

--- sync/views.py
+++ sync/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def welcome(request):
-    return render(
-        request,
-        "sync/welcome.html",
-    )
+    if request.method == "POST":
+        print(request)
+    else:
+        return render(request, "sync/welcome.html", )
 
 
