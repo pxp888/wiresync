fed176cb689965df5993d6f019f8a28ee60cdebf
pxp888 <pxperrine@gmail.com>
2024-01-27 01:19:12 +0100

    add welcome page

--- sync/views.py
+++ sync/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def welcome(request):
-    return render(request, 'sync/welcome.html')
+    return render(
+        request,
+        "sync/welcome.html",
+    )
+
 
--- wiresync/settings.py
+++ wiresync/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sync',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
